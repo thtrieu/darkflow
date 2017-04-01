5a1d428c2108a15035247e8359f9e159abcc4fe2
Dhruv Mohan <dhruv.mohan@outlook.com>
2017-04-01 21:01:13 +0530

    windows specifc fix

--- cython_utils/setup.py
+++ cython_utils/setup.py
@@ -1,19 +1,77 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import numpy
+import os
 
+if os.name=='nt' :
 
-ext_modules=[
-    Extension("cy_yolo2_findboxes",
-              sources=["cy_yolo2_findboxes.pyx"],
-              libraries=["m"] # Unix-like specific
-    )
-]
+    ext_modules=[
+        Extension("nms",
+                sources=["nms.pyx"],
+                #libraries=["m"] # Unix-like specific
+                    include_dirs=[numpy.get_include()]
+        )
+    ]
+
+    ext_modules_yolo2=[
+        Extension("cy_findboxes",
+                  sources=["cy_yolo2_findboxes.pyx"],
+                  #libraries=["m"] # Unix-like specific
+                  include_dirs=[numpy.get_include()]
+        )
+    ]
+
+    ext_modules_yolo=[
+        Extension("cy_findboxes_yolo",
+                  sources=["cy_yolo_findboxes.pyx"],
+                  #libraries=["m"] # Unix-like specific
+                  include_dirs=[numpy.get_include()]
+        )
+    ]
+
+else :
+    
+    ext_modules=[
+        Extension("nms",
+                sources=["nms.pyx"],
+                libraries=["m"] # Unix-like specific
+        )
+    ]
+
+    ext_modules_yolo2=[
+        Extension("cy_findboxes",
+                  sources=["cy_yolo2_findboxes.pyx"],
+                  libraries=["m"] # Unix-like specific
+        )
+    ]
+
+    ext_modules_yolo=[
+        Extension("cy_findboxes_yolo",
+                  sources=["cy_yolo_findboxes.pyx"],
+                  libraries=["m"] # Unix-like specific
+        )
+    ]
+
 
 
 
 setup(
 
-    name= 'cy_yolo2_findboxes',
+    #name= 'cy_findboxes',
     ext_modules = cythonize(ext_modules),
+)
+
+setup(
+
+    #name= 'cy_findboxes',
+    ext_modules = cythonize(ext_modules_yolo2),
+)
+
+
+
+setup(
+
+    #name= 'cy_findboxes',
+    ext_modules = cythonize(ext_modules_yolo),
 )
