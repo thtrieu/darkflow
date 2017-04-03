d2110750e7f02eb8c2dbaeb351aa026ee680ec3c
thtrieu <thtrieu@apcs.vn>
2017-04-03 10:09:07 +0700

    delete comment

--- cython_utils/nms.pyx
+++ cython_utils/nms.pyx
@@ -59,36 +59,7 @@ cdef float box_iou_c(float ax, float ay, float aw, float ah, float bx, float by,
 #NMS
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
-@cython.cdivision(True)
-# cdef NMS(float[:, ::1] final_probs , float[:, ::1] final_bbox):
-#     cdef list boxes = list()
-#     cdef:
-#         np.intp_t pred_length,class_length,class_loop,index,index2
-
-  
-#     pred_length = final_bbox.shape[0]
-#     class_length = final_probs.shape[1]
-#     for class_loop in range(class_length):
-#         for index in range(pred_length):
-#             if final_probs[index,class_loop] == 0: continue
-#             for index2 in range(index+1,pred_length):
-#                 if final_probs[index2,class_loop] == 0: continue
-#                 if index==index2 : continue
-#                 if box_iou_c(final_bbox[index,0],final_bbox[index,1],final_bbox[index,2],final_bbox[index,3],final_bbox[index2,0],final_bbox[index2,1],final_bbox[index2,2],final_bbox[index2,3]) >= 0.4:
-#                     if final_probs[index2,class_loop] > final_probs[index, class_loop] :
-#                         final_probs[index, class_loop] =0
-#                         break
-#                     final_probs[index2,class_loop]=0
-#             bb=BoundBox(class_length)
-#             bb.x = final_bbox[index, 0]
-#             bb.y = final_bbox[index, 1]
-#             bb.w = final_bbox[index, 2]
-#             bb.h = final_bbox[index, 3]
-#             bb.c = final_bbox[index, 4]
-#             bb.probs = np.asarray(final_probs[index,:])
-#             boxes.append(bb)
-  
-#     return boxes
+@cython.cdivision(True)]
 cdef NMS(float[:, ::1] final_probs , float[:, ::1] final_bbox):
     cdef list boxes = list()
     cdef:
