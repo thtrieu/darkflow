be689e726e717bb0c4ed2efc0bdb4ca0e90d9b31
Richard Price-Jones <richjones_07@hotmail.co.uk>
2018-03-26 02:28:48 +0100

    Started changing the netwok for yopo

--- darkflow/net/yolo/data.py
+++ darkflow/net/yolo/data.py
@@ -5,7 +5,7 @@ from .predict import preprocess
 from copy import deepcopy
 import pickle
 import numpy as np
-import os 
+import os
 
 def parse(self, exclusive = False):
     meta = self.meta
@@ -22,7 +22,7 @@ def parse(self, exclusive = False):
 def _batch(self, chunk):
     """
     Takes a chunk of parsed annotations
-    returns value for placeholders of net's 
+    returns value for placeholders of net's
     input & loss layer correspond to this chunk
     """
     meta = self.meta
@@ -31,18 +31,28 @@ def _batch(self, chunk):
 
     # preprocess
     jpg = chunk[0]; w, h, allobj_ = chunk[1]
+
     allobj = deepcopy(allobj_)
     path = os.path.join(self.FLAGS.dataset, jpg)
     img = self.preprocess(path, allobj)
-
+    print("ALL object: ", allobj)
     # Calculate regression target
+    # Which cell is the object centre in.
+
+    # Find width a of single cell.
+    print("Cell size", S)
     cellx = 1. * w / S
     celly = 1. * h / S
+    print("Cell:", cellx, celly)
     for obj in allobj:
+        # print(obj)
         centerx = .5*(obj[1]+obj[3]) #xmin, xmax
         centery = .5*(obj[2]+obj[4]) #ymin, ymax
         cx = centerx / cellx
         cy = centery / celly
+        print("Centre point x:{}, y:{}".format(centerx, centery))
+        print("cx:{}, cy:{}".format(cx,cy))
+
         if cx >= S or cy >= S: return None, None
         obj[3] = float(obj[3]-obj[1]) / w
         obj[4] = float(obj[4]-obj[2]) / h
@@ -55,38 +65,48 @@ def _batch(self, chunk):
     # show(im, allobj, S, w, h, cellx, celly) # unit test
 
     # Calculate placeholders' values
+    # print(C)
+    # print(B)
+    # print(labels)
+
     probs = np.zeros([S*S,C])
     confs = np.zeros([S*S,B])
     coord = np.zeros([S*S,B,4])
     proid = np.zeros([S*S,C])
     prear = np.zeros([S*S,4])
+    # 6 is the prob score?
+    print("************************")
     for obj in allobj:
-        probs[obj[5], :] = [0.] * C
-        probs[obj[5], labels.index(obj[0])] = 1.
-        proid[obj[5], :] = [1] * C
-        coord[obj[5], :, :] = [obj[1:5]] * B
-        prear[obj[5],0] = obj[1] - obj[3]**2 * .5 * S # xleft
-        prear[obj[5],1] = obj[2] - obj[4]**2 * .5 * S # yup
-        prear[obj[5],2] = obj[1] + obj[3]**2 * .5 * S # xright
-        prear[obj[5],3] = obj[2] + obj[4]**2 * .5 * S # ybot
-        confs[obj[5], :] = [1.] * B
+        print("obj", obj)
+
+        probs[obj[6], :] = [0.] * C
+        probs[obj[6], labels.index(obj[0])] = 1.
+        proid[obj[6], :] = [1] * C
+        coord[obj[6], :, :] = [obj[1:5]] * B
+        prear[obj[6],0] = obj[1] - obj[3]**2 * .5 * S # xleft
+        prear[obj[6],1] = obj[2] - obj[4]**2 * .5 * S # yup
+        prear[obj[6],2] = obj[1] + obj[3]**2 * .5 * S # xright
+        prear[obj[6],3] = obj[2] + obj[4]**2 * .5 * S # ybot
+        # prear[obj[6],4] = obj[]
+        confs[obj[6], :] = [1.] * B
 
     # Finalise the placeholders' values
     upleft   = np.expand_dims(prear[:,0:2], 1)
     botright = np.expand_dims(prear[:,2:4], 1)
-    wh = botright - upleft; 
+    wh = botright - upleft;
     area = wh[:,:,0] * wh[:,:,1]
     upleft   = np.concatenate([upleft] * B, 1)
     botright = np.concatenate([botright] * B, 1)
     areas = np.concatenate([area] * B, 1)
+    # angle = np.concatenate()
 
     # value for placeholder at input layer
     inp_feed_val = img
-    # value for placeholder at loss layer 
+    # value for placeholder at loss layer
     loss_feed_val = {
-        'probs': probs, 'confs': confs, 
+        'probs': probs, 'confs': confs,
         'coord': coord, 'proid': proid,
-        'areas': areas, 'upleft': upleft, 
+        'areas': areas, 'upleft': upleft,
         'botright': botright
     }
 
@@ -123,14 +143,13 @@ def shuffle(self):
 
                 for key in new_feed:
                     new = new_feed[key]
-                    old_feed = feed_batch.get(key, 
+                    old_feed = feed_batch.get(key,
                         np.zeros((0,) + new.shape))
-                    feed_batch[key] = np.concatenate([ 
-                        old_feed, [new] 
-                    ])      
-            
+                    feed_batch[key] = np.concatenate([
+                        old_feed, [new]
+                    ])
+
             x_batch = np.concatenate(x_batch, 0)
             yield x_batch, feed_batch
-        
-        print('Finish {} epoch(es)'.format(i + 1))
 
+        print('Finish {} epoch(es)'.format(i + 1))
