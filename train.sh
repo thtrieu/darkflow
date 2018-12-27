8c05321848ce0c5749e6e8493c22886062bccc3b
MihaiPerju <mihaiperju98@gmail.com>
2018-12-27 00:50:37 +0000

    finished setup

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-flow --model cfg/yolo-new.cfg --load bin/tiny-yolo-voc.weights --train --annotation pepsi_data/annotations --dataset pepsi_data/images/ --gpu 0.8 --epoch 300
+flow --model cfg/yolo-new.cfg --load bin/tiny-yolo-voc.weights --train --annotation pepsi_data/annotations --dataset pepsi_data/images/ --gpu 1.0 --epoch 300
